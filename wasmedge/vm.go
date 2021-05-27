c802305bbcdf8f19537f4b9ce1a97c6bd7865f72
YiYing He <yiying@secondstate.io>
2021-05-28 00:02:25 +0800

    [Package] Add bindgen APIs implementation.

--- wasmedge/vm.go
+++ wasmedge/vm.go
@@ -7,14 +7,21 @@ const char *_GoStringPtr(_GoString_ s);
 */
 import "C"
 import (
-	"io/ioutil"
-	"os"
 	"unsafe"
 )
 
 type VM struct {
 	_inner *C.WasmEdge_VMContext
 }
+
+type bindgen int
+
+const (
+	Bindgen_return_void  bindgen = iota
+	Bindgen_return_i32   bindgen = iota
+	Bindgen_return_i64   bindgen = iota
+	Bindgen_return_array bindgen = iota
+)
 
 func NewVM() *VM {
 	self := &VM{
@@ -99,34 +106,6 @@ func (self *VM) RunWasmFile(path string, funcname string, params ...interface{})
 	return self.runWasm(funcname, params...)
 }
 
-func (self *VM) RunWasmFileWithDataAndWASI(path string, funcname string, data []byte,
-	args []string, envp []string, dirs []string, preopens []string, params ...interface{}) ([]interface{}, error) {
-	/// Create a temp file
-	tmpf, _ := ioutil.TempFile("", "tmp.*.bin")
-	defer os.Remove(tmpf.Name())
-	tmpf.Write(data)
-	tmpf.Close()
-
-	/// Init WASI (test)
-	var wasi = self.GetImportObject(WASI)
-	if wasi != nil {
-		wasi.InitWasi(
-			args, /// The args
-			append(envp, "WasmEdge_DATA_TO_CALLEE="+tmpf.Name()), /// The envs
-			append(dirs, "/tmp:/tmp"),                            /// The mapping directories
-			preopens,                                             /// The preopens will be empty
-		)
-	}
-
-	cpath := C.CString(path)
-	defer C.free(unsafe.Pointer(cpath))
-	res := C.WasmEdge_VMLoadWasmFromFile(self._inner, cpath)
-	if !C.WasmEdge_ResultOK(res) {
-		return nil, newError(res)
-	}
-	return self.runWasm(funcname, params...)
-}
-
 func (self *VM) RunWasmBuffer(buf []byte, funcname string, params ...interface{}) ([]interface{}, error) {
 	res := C.WasmEdge_VMLoadWasmFromBuffer(self._inner, (*C.uint8_t)(unsafe.Pointer(&buf)), C.uint32_t(len(buf)))
 	if !C.WasmEdge_ResultOK(res) {
@@ -185,6 +164,29 @@ func (self *VM) Execute(funcname string, params ...interface{}) ([]interface{}, 
 	return fromWasmEdgeValueSlide(creturns, ftype._returns), nil
 }
 
+// Special execute function for running with wasm-bindgen.
+func (self *VM) ExecuteBindgen(funcname string, rettype bindgen, params ...interface{}) (interface{}, error) {
+	funcstr := toWasmEdgeStringWrap(funcname)
+	ftype := self.GetFunctionType(funcname)
+	retarray := rettype == Bindgen_return_array
+	cparams := toWasmEdgeValueSlideBindgen(self, retarray, nil, params...)
+	creturns := make([]C.WasmEdge_Value, len(ftype._returns))
+	var ptrparams *C.WasmEdge_Value = nil
+	var ptrreturns *C.WasmEdge_Value = nil
+	if len(cparams) > 0 {
+		ptrparams = (*C.WasmEdge_Value)(unsafe.Pointer(&cparams[0]))
+	}
+	if len(creturns) > 0 {
+		ptrreturns = (*C.WasmEdge_Value)(unsafe.Pointer(&creturns[0]))
+	}
+
+	res := C.WasmEdge_VMExecute(self._inner, funcstr, ptrparams, C.uint32_t(len(cparams)), ptrreturns, C.uint32_t(len(creturns)))
+	if !C.WasmEdge_ResultOK(res) {
+		return nil, newError(res)
+	}
+	return fromWasmEdgeValueSlideBindgen(self, rettype, nil, creturns, ftype._returns)
+}
+
 func (self *VM) ExecuteRegistered(modname string, funcname string, params ...interface{}) ([]interface{}, error) {
 	modstr := toWasmEdgeStringWrap(modname)
 	funcstr := toWasmEdgeStringWrap(funcname)
@@ -204,6 +206,30 @@ func (self *VM) ExecuteRegistered(modname string, funcname string, params ...int
 		return nil, newError(res)
 	}
 	return fromWasmEdgeValueSlide(creturns, ftype._returns), nil
+}
+
+// Special execute function for running with wasm-bindgen.
+func (self *VM) ExecuteBindgenRegistered(modname string, funcname string, rettype bindgen, params ...interface{}) (interface{}, error) {
+	modstr := toWasmEdgeStringWrap(modname)
+	funcstr := toWasmEdgeStringWrap(funcname)
+	ftype := self.GetFunctionType(funcname)
+	retarray := rettype == Bindgen_return_array
+	cparams := toWasmEdgeValueSlideBindgen(self, retarray, &modname, params...)
+	creturns := make([]C.WasmEdge_Value, len(ftype._returns))
+	var ptrparams *C.WasmEdge_Value = nil
+	var ptrreturns *C.WasmEdge_Value = nil
+	if len(cparams) > 0 {
+		ptrparams = (*C.WasmEdge_Value)(unsafe.Pointer(&cparams[0]))
+	}
+	if len(creturns) > 0 {
+		ptrreturns = (*C.WasmEdge_Value)(unsafe.Pointer(&creturns[0]))
+	}
+
+	res := C.WasmEdge_VMExecuteRegistered(self._inner, modstr, funcstr, ptrparams, C.uint32_t(len(cparams)), ptrreturns, C.uint32_t(len(creturns)))
+	if !C.WasmEdge_ResultOK(res) {
+		return nil, newError(res)
+	}
+	return fromWasmEdgeValueSlideBindgen(self, rettype, &modname, creturns, ftype._returns)
 }
 
 func (self *VM) GetFunctionType(funcname string) *FunctionType {
